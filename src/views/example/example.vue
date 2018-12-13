883a55463e7e833ee3fdeaa1bb6eb52a9309bb75
jimmy <tliang92@gmail.com>
2018-12-13 14:14:27 +0800

    jkfdjk

---
+++ src/views/example/example.vue
@@ -0,0 +1,8 @@
+<!-- <template>
+	<div class="example">this is example</div>
+</template>
+<style>
+	.example {
+		color: red;
+	}
+</style> -->
---
+++ src/views/example/example1.vue
@@ -0,0 +1,8 @@
+<!-- <template>
+	<div class="example">this is example1</div>
+</template>
+<style>
+	.example {
+		color: gray;
+	}
+</style> -->
